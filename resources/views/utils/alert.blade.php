c714d88a54309ce50e988d72fdca1c68031081ab
Elvio Cali <ecali@gmail.com>
2020-07-03 10:51:38 +0200

    Add delete button and Handle deleted task view

--- resources/views/utils/alert.blade.php
+++ resources/views/utils/alert.blade.php
@@ -4,18 +4,30 @@
         <div class="alert alert-primary" role="alert">
     @endif
     <h5><strong>{{$data->title}}</strong>
-        <form action = "{{route('taskUpdate')}}" method = "post" style="float: right">
-            @csrf
+
         @if($data->do == 0)
-            <input type="hidden" name="id" value="0">
-            <input type="hidden" name="idTask" value="{{$data->id}}">
-            <button class="btn" type="submit"><span class="material-icons">loop</span></button>
-        @else
+            <form action = "{{route('taskUpdate')}}" method = "post" style="float: right">
+                @csrf
+                <input type="hidden" name="id" value="0">
+                <input type="hidden" name="idTask" value="{{$data->id}}">
+                <button class="btn" type="submit"><span class="material-icons">loop</span></button>
+            </form>
+                <form action = "{{route('taskDelete')}}" method = "post" style="float: right">
+                    @csrf
+                    <input type="hidden" name="idTaskd" value="{{$data->id}}">
+                    <button class="btn border-danger" type="submit"><span class="material-icons">delete</span></button>
+                </form>
+            @else
+            <form action = "{{route('taskUpdate')}}" method = "post" style="float: right">
+                @csrf
             <input type="hidden" name="id" value="1">
                 <input type="hidden" name="idTask" value="{{$data->id}}">
                 <button class="btn" type="submit"><span class="material-icons">done_all</span></button>
+            </form>
         @endif
-        </form>
+
+
+
 
     </h5>
     <hr>
